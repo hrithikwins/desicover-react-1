2159ddd73b312de461a9b083c3fec876ad2b7848
Hrithik Tiwari <javaoncloud14@gmail.com>
2022-02-05 16:37:42 +0530

    some changes

--- homepage/pages/_app.js
+++ homepage/pages/_app.js
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "../styles/product-card-styles.sass";
 import "bootstrap/dist/css/bootstrap.css";
+import "swiper/css";
+import ProductProvider from "../pages/context/product-context";
+import { CartProvider } from "react-use-cart";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <CartProvider>
+            <ProductProvider>
+                <Component {...pageProps} />
+            </ProductProvider>
+        </CartProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
